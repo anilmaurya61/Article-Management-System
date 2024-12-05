ed827a325ca6346ed61b8d31b108b5aa58e31b61
anilsigtuple <anil.maurya@sigtuple.com>
2024-12-05 17:02:48 +0530

    added templates

--- artical_management_project/settings.py
+++ artical_management_project/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'users',
     'articles',
-    'api',
 ]
 
 AUTH_USER_MODEL = 'users.User'
--- artical_management_project/urls.py
+++ artical_management_project/urls.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('api/', include('users.urls')),  # Include the 'users' app URLs
     path('api/', include('articles.urls')), # Include the 'users' app URLs
+
+    # Frontend Pages:
+    path('login/', TemplateView.as_view(template_name='templates/users/login.html'), name='login'),
+    path('register/', TemplateView.as_view(template_name='users/register.html'), name='register'),
+    path('article/list/', TemplateView.as_view(template_name='articles/list.html'), name='article_list'),
+    path('article/create/', TemplateView.as_view(template_name='articles/create.html'), name='article_create'),
+    path('article/edit/<int:id>/', TemplateView.as_view(template_name='articles/edit.html'), name='article_edit'),
+
 ]
